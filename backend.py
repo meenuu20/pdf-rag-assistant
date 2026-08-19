afb7565b3625a329bc89ac516b9c43a69ca1aabe
Meenu <meenukrishnaks05@gmail.com>
2026-08-19 14:57:45 +0530

    Prepare backend for deployment

--- backend.py
+++ backend.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, UploadFile, File
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -10,7 +11,7 @@ current_chunks=[]
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -44,7 +45,14 @@ async def upload_file(file: UploadFile = File(...)):
         return{
             "message" : "Invalid file type. Please upload a PDF file."
         }
-    file_path = f"uploads/{file.filename}"
+    os.makedirs("uploads", exist_ok=True)
+
+    filename = os.path.basename(file.filename)
+
+    file_path = os.path.join(
+        "uploads",
+        filename
+    )
 
     with open(file_path, "wb") as buffer:
         buffer.write(await file.read())
